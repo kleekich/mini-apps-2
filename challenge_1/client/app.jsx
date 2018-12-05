9610e4710d15b27c7ba118d623f7ae62497d756d
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-12-05 13:33:20 -0800

    Let jason-server to serve static file

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import List from './src/components/List.jsx';
 
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Hi from React</h1>
+        {/* <List />  */}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('App'));
 
